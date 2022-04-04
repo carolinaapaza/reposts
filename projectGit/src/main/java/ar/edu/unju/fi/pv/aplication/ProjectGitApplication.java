bc1309d728effb3f11c1f686ab261fb7e6acbc9c
Carolina Apaza <carolinaapaza@hotmail.com>
2022-04-04 13:47:11 -0300

    primer commit

---
+++ projectGit/src/main/java/ar/edu/unju/fi/pv/aplication/ProjectGitApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pv.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGitApplication.class, args);
+	}
+
+}
---
+++ projectGit/src/test/java/ar/edu/unju/fi/pv/aplication/ProjectGitApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pv.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
